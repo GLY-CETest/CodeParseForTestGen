482b614908d9289f00579bf1dd4fda282e5a2006
GLY-CETest <yigaolei666@163.com>
2024-06-12 14:17:10 +0800

    update

--- src/main/java/cn/iselab/mutant/process/MutantFinder.java
+++ src/main/java/cn/iselab/mutant/process/MutantFinder.java
@@ -2,17 +2,24 @@ package cn.iselab.mutant.process;
 
 import org.json.JSONObject;
 
+import java.util.Iterator;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
 public class MutantFinder {
     public static void main(String[] args) throws Exception {
         String filePath = "C:\\YGL\\Projects\\pythonProject\\MutationTestGEN-LLM\\projUT\\Triangle\\target\\mutants\\1\\details.json";
+        String astPath = "C:\\YGL\\Projects\\pythonProject\\MutationTestGEN-LLM\\projUT\\Triangle\\target\\parsefiles\\ast_json\\Triangle.json";
         int lineNumber = mutantLineNumberFinder(filePath);
         System.out.println("Mutant lineNumber: " + lineNumber);
+        mutantMethodFinder(astPath, lineNumber);
     }
 
     public static int mutantLineNumberFinder(String path) throws Exception {
@@ -35,6 +42,41 @@ public class MutantFinder {
         }
     }
 
-//    public static String mutantMethodFinder(String path) throws Exception {
-//    }
+    public static String mutantMethodFinder(String path, int lineNumber) throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            // 读取 JSON 文件
+            File jsonFile = new File(path);
+            JsonNode jsonNode = objectMapper.readTree(jsonFile);
+
+            // 确保读取的是数组
+            if (jsonNode.isArray()) {
+                // 遍历数组中的每个元素
+                for (JsonNode elementNode : jsonNode) {
+                    // 处理每个 JSON 对象
+                    String name = elementNode.get("name").asText();
+                    String type = elementNode.get("type").asText();
+                    if (type.equals("method")) {
+                        int start_line = elementNode.get("start_line").asInt();
+                        int end_line = elementNode.get("end_line").asInt();
+                        // ... 根据需要读取更多字段
+                        if (lineNumber >= start_line && lineNumber <= end_line)
+                        {
+                            System.out.println("name: " + name);
+                            return name;
+                        }
+                        else
+                            continue;
+                    }
+                    else
+                        continue;
+                }
+            } else {
+                System.out.println("Root is not an array.");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
